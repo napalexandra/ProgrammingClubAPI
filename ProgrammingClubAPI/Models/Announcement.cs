bcf9e9930ff762af9c366ade20d0f168cf87f558
msoft6937@myoffice365.site <msoft6937@myoffice365.site>
2025-05-23 14:21:01 +0300

    Remove some extra

--- ProgrammingClubAPI/Models/Announcement.cs
+++ ProgrammingClubAPI/Models/Announcement.cs
@@ -6,20 +6,11 @@ namespace ProgrammingClubAPI.Models
     {
         [Key]
         public Guid? IdAnnouncement { get; set; }
-
         public DateTime? ValidFrom { get; set; }
-
         public DateTime? ValidTo { get; set; }
-
-        [StringLength(250, ErrorMessage = "Title's maximus size is 250")]
         public string? Title { get; set; }
-
-        [StringLength(1000, ErrorMessage = "Text's maximus size is 1000")]
         public string? Text { get; set; }
-
         public DateTime? EventDate { get; set; }
-
-        [StringLength(1000, ErrorMessage = "Tags's maximus size is 1000")]
         public string? Tags { get; set; }
     }
 }
--- ProgrammingClubAPI/Models/CodeSnippet.cs
+++ ProgrammingClubAPI/Models/CodeSnippet.cs
@@ -6,19 +6,11 @@ namespace ProgrammingClubAPI.Models
     {
         [Key]
         public Guid? IdCodeSnippet { get; set; }
-
-        [StringLength(100, ErrorMessage = "Title's maximus size is 100")]
         public string? Title { get; set; }
-
         public Guid? IdMember { get; set; }
-
-        [Range(0, int.MaxValue, ErrorMessage = "Revision must be a positive number")]
         public int? Revision { get; set; }
-
         public Guid? IdSnippetPreviousVersion { get; set; }
-
         public DateTime? DateTimeAdded { get; set; }
-
         public bool? isPublished { get; set; }
     }
 }
